d0d13cde27ec203900023e6df63de4615bfe939a
smangaliso <smangazuma4@gmail.com>
2022-07-07 09:38:10 +0200

    first commit

--- flask-server/app/models.py
+++ flask-server/app/models.py
@@ -2,6 +2,9 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+import itertools
+import json
+import urllib.request
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -60,3 +63,63 @@ cases_schema = CasesSchema(many=True)
 
 vaccine_schema = VaccinesSchema()
 vaccines_schema = VaccinesSchema(many=True)
+
+
+def get_api_data(name):
+    url = "https://covid-api.mmediagroup.fr/v1/" + name
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    to_dict = json.loads(data)
+    to_dict = dict(itertools.islice(to_dict.items(), 10))
+
+    return to_dict
+
+
+def upsert_vaccines():
+    vaccines = get_api_data('vaccines')
+
+    for key in vaccines.keys():
+        country = key
+
+        try:
+            administered = vaccines[country]['All']['administered']
+            people_vaccinated = vaccines[country]['All']['people_vaccinated']
+            people_partially_vaccinated = vaccines[country]['All']['people_partially_vaccinated']
+
+            vaccine = VaccineModel(country, administered, people_vaccinated, people_partially_vaccinated)
+            db.session.add(vaccine)
+            db.session.commit()
+
+        except:
+            db.session.rollback()
+            vaccine = VaccineModel.query.filter('country' == country)
+            administered = vaccines[country]['All']['administered']
+            people_vaccinated = vaccines[country]['All']['people_vaccinated']
+            people_partially_vaccinated = vaccines[country]['All']['people_partially_vaccinated']
+
+            vaccine.administered = administered
+            vaccine.people_vaccinated = people_vaccinated
+            vaccine.people_partially_vaccinated = people_partially_vaccinated
+            db.session.commit()
+
+
+def upsert_cases():
+    cases = get_api_data('cases')
+
+    for key in cases.keys():
+        country = key
+
+        try:
+            confirmed = cases[country]['All']['confirmed']
+            death = cases[country]['All']['deaths']
+            case = CasesModel(country, confirmed, death)
+            db.session.add(case)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            case = CasesModel.query.filter('country' == country)
+            confirmed = cases[country]['All']['confirmed']
+            death = cases[country]['All']['deaths']
+            case.confirmed = confirmed
+            case.death = death
+            db.session.commit()
